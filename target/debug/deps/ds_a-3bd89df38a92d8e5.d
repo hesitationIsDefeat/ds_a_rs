cbfeb4e390f9983ee646cfdaae3255176c2bb617
hesitationIsDefeat <onat@Onats-MacBook-Pro.local>
2025-07-25 13:40:33 +0300

    Remove ignored files from tracking

--- target/debug/deps/ds_a-3bd89df38a92d8e5.d
+++
@@ -1,8 +0,0 @@
-/Users/onat/Desktop/Code/Rust/ds_a/target/debug/deps/ds_a-3bd89df38a92d8e5.d: src/lib.rs src/ds/mod.rs src/ds/list/mod.rs src/ds/list/singly.rs
-
-/Users/onat/Desktop/Code/Rust/ds_a/target/debug/deps/libds_a-3bd89df38a92d8e5.rmeta: src/lib.rs src/ds/mod.rs src/ds/list/mod.rs src/ds/list/singly.rs
-
-src/lib.rs:
-src/ds/mod.rs:
-src/ds/list/mod.rs:
-src/ds/list/singly.rs:
--- target/debug/deps/ds_a-eeb57f2a91b1854f.d
+++
@@ -1,8 +0,0 @@
-/Users/onat/Desktop/Code/Rust/ds_a/target/debug/deps/ds_a-eeb57f2a91b1854f.d: src/lib.rs src/ds/mod.rs src/ds/list/mod.rs src/ds/list/singly.rs
-
-/Users/onat/Desktop/Code/Rust/ds_a/target/debug/deps/ds_a-eeb57f2a91b1854f: src/lib.rs src/ds/mod.rs src/ds/list/mod.rs src/ds/list/singly.rs
-
-src/lib.rs:
-src/ds/mod.rs:
-src/ds/list/mod.rs:
-src/ds/list/singly.rs:
